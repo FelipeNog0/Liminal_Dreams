d3b9865969ceb167d1deb3e15efa9bdf1e9b29d3
Felipe Nogeuira <felipenogueira@skyfix.com.br>
2025-07-29 15:42:37 -0300

    PlayerManager

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -5,11 +5,18 @@ public class PlayerManager : MonoBehaviour
     [Header("Movimentação")]
     public float moveSpeed = 5f;
 
-    [Header("Mouse Look")]
+    [Header("Mouse_Movimentação")]
     public float mouseSensitivity = 100f;
     public Transform cameraTransform;
 
+    private float bobFrequency = 10f;
+    private float bobAmplitude = 0.05f;
+    private float bobTimer = 0f;
+    private Vector3 cameraInitialLocalPos;
+    private Rigidbody rigidbody;
+
     float xRotation = 0f;
+    public float corridavelocidade = 10f;
 
     void Start()
     {
@@ -17,10 +24,18 @@ public class PlayerManager : MonoBehaviour
         Cursor.visible = false;
     }
 
+    private void Awake()
+    {
+        cameraInitialLocalPos = cameraTransform.localPosition;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
+    }
+
     void Update()
     {
         Mover();
         OlharComMouse();
+        CameraBalanco();
     }
 
     void Mover()
@@ -29,6 +44,7 @@ public class PlayerManager : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
 
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? corridavelocidade : moveSpeed;
         Vector3 move = transform.TransformDirection(direction) * moveSpeed * Time.deltaTime;
 
         transform.position += move;
@@ -48,4 +64,29 @@ public class PlayerManager : MonoBehaviour
         transform.Rotate(Vector3.up * mouseX);
     }
 
+    void CameraBalanco()
+    {
+        float bobSpeed = Input.GetKey(KeyCode.LeftShift) ? bobFrequency * 1.5f : bobFrequency;
+        float bobSize = Input.GetKey(KeyCode.LeftShift) ? bobAmplitude * 1.5f : bobAmplitude;
+        float speed = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).magnitude;
+        if (speed > 0.1f && CharacterEstáNoChão())
+        {
+            bobTimer += Time.deltaTime * bobFrequency;
+            float bobOffset = Mathf.Sin(bobTimer) * bobAmplitude;
+            cameraTransform.localPosition = cameraInitialLocalPos + new Vector3(0f, bobOffset, 0f);
+        }
+        else
+        {
+            bobTimer = 0f;
+            cameraTransform.localPosition = Vector3.Lerp(cameraTransform.localPosition, cameraInitialLocalPos, Time.deltaTime * 5f);
+        }
+    }
+
+    private float groundCheckDistance = 1.1f;
+    public LayerMask groundMask;
+
+    bool CharacterEstáNoChão()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundMask);
+    }
 }
